fb0ac1fd1606bfd9eeb109ef1b16fd2dbf56eb09
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-04-15 23:11:01 +0700

    F I N I S H E D

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("*",(req,res,next)=> {
 })
 
 //routes
-app.get('/',,indexController)
+app.get('/',indexController)
 app.get('/login',redirectIfAuth,loginController)
 app.get('/register',redirectIfAuth,registerController)
 app.post('/user/register',redirectIfAuth, storeUser)
